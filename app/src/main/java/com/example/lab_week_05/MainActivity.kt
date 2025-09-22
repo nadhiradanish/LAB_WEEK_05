335727d63c36a3e4e8bb764fd45fc9ea734f34f2
nadhiradanish <152785547+nadhiradanish@users.noreply.github.com>
2025-09-22 12:00:02 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -2,48 +2,43 @@ package com.example.lab_week_05
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.lab_week_05.api.CatApiService
+import com.example.lab_week_05.model.ImageData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import com.example.lab_week_05.api.CatApiService
-import com.example.lab_week_05.model.ImageData
 import retrofit2.converter.moshi.MoshiConverterFactory
-
-
 
 class MainActivity : AppCompatActivity() {
 
-    // view TextView
+
+    private val catApiService by lazy {
+        retrofit.create(CatApiService::class.java)
+    }
+
     private val apiResponseView: TextView by lazy {
         findViewById(R.id.api_response)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // apply insets ke root view
-        val rootView: View = findViewById(android.R.id.content)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // panggil API
-        getCatImageResponse()
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
     }
 
-    private val retrofit by lazy{
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
+
+    }
+
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
             .addConverterFactory(MoshiConverterFactory.create())
@@ -51,28 +46,46 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private val catApiService by lazy {
-        retrofit.create(CatApiService::class.java)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val rootView: View = findViewById(android.R.id.content)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        getCatImageResponse()
     }
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object: Callback<List<ImageData>> {
+        call.enqueue(object : Callback<List<ImageData>> {
             override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
 
-            override fun onResponse(call: Call<List<ImageData>>,
-                                    response: Response<List<ImageData>>) {
-                if(response.isSuccessful){
+            override fun onResponse(
+                call: Call<List<ImageData>>,
+                response: Response<List<ImageData>>
+            ) {
+                if (response.isSuccessful) {
                     val image = response.body()
                     val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
-                    apiResponseView.text = getString(R.string.image_placeholder,
-                        firstImage)
-                }
-                else{
-                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
-                            response.errorBody()?.string().orEmpty()
+                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
+
+                    // pakai Glide langsung
+                    Glide.with(this@MainActivity)
+                        .load(firstImage)
+                        .into(imageResultView)
+                } else {
+                    Log.e(
+                        MAIN_ACTIVITY, "Failed to get response\n" +
+                                response.errorBody()?.string().orEmpty()
                     )
                 }
             }
